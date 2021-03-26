d62fd55aa596eedd9b23fed7a4c756ce8f89fc84
张世良 <zsldqs883300@126.com>
2021-03-26 15:57:20 +0800

    first commit

---
+++ SdkTest/Rtm.framework/Headers/RTMVoiceEnterRoomTokenAnswer.h
@@ -0,0 +1,17 @@
+//
+//  RTMVoiceEnterVoiceAnswer.h
+//  Rtm
+//
+//  Created by zsl on 2021/1/5.
+//  Copyright © 2021 FunPlus. All rights reserved.
+//
+
+#import "RTMBaseAnswer.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RTMVoiceEnterRoomTokenAnswer : RTMBaseAnswer
+@property(nonatomic,strong)NSString * token;
+@end
+
+NS_ASSUME_NONNULL_END
